344e5ef2194903a6e8b2d9bc051120f55d4fae64
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-09-21 22:56:59 +0530

    Add Global Styles for Dark/Light Theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,14 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 // Styled Component
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 import { normalize } from "styled-normalize"
 
 // Global Styles
 const GlobalStyles = createGlobalStyle`
+
+  ${normalize}
+
   * {
     text-decoration: none;
   }
@@ -22,9 +25,20 @@ const GlobalStyles = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     overscroll-behavior: none;
     overflow-x: hidden;
-    background: #fff;
+    background: ${props => props.theme.background};
+    color: ${props => props.theme.color};
   }
 `
+
+const darkTheme = {
+  background: "#121212",
+  color: "#FAFAFA",
+}
+
+const lightTheme = {
+  background: "#FAFAFA",
+  color: "#121212",
+}
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -38,10 +52,10 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <ThemeProvider theme={lightTheme}>
       <GlobalStyles />
       <main>{children}</main>
-    </>
+    </ThemeProvider>
   )
 }
 
