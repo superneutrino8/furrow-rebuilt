635af3c7746145a2d5657548c9bf846cab3ca655
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-09-21 22:39:40 +0530

    Add Global CSS Styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,30 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+// Styled Component
+import { createGlobalStyle } from "styled-components"
+import { normalize } from "styled-normalize"
+
+// Global Styles
+const GlobalStyles = createGlobalStyle`
+  * {
+    text-decoration: none;
+  }
+
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    overscroll-behavior: none;
+    overflow-x: hidden;
+    background: #fff;
+  }
+`
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -13,7 +37,12 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  return (
+    <>
+      <GlobalStyles />
+      <main>{children}</main>
+    </>
+  )
 }
 
 Layout.propTypes = {
