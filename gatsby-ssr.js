59ebaad60a1b616928eddc4dac95827e611736e6
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-10-08 16:36:22 +0530

    Fix context issue

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+
+// Context
+import { GlobalProvider } from "./src/context/globalContext"
+
+export const wrapRootElement = ({ element }) => {
+  return <GlobalProvider>{element}</GlobalProvider>
+}
